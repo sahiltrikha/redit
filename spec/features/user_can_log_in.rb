f0624e316fc81418d7c68cc34c99c5e3f2824a48
Tahira <tahira.adaya@gmail.com>
2014-02-28 16:07:53 -0500

    user can log in feature test

---
+++ spec/features/user_can_log_in.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+descrive "user can log in" do 
+
+  it "can enter user and pass to log in" do
+    visit root_path
+    click_link "Log In"
+    fill :username, with: user.username
+    fill_in :password, user.password
+    click_button "Log In"
+
+    expect(page).to have_content user.username
+  end
+
+end
+
+
+
+    
--- spec/features/user_can_submit_link.rb
+++ spec/features/user_can_submit_link.rb
@@ -3,4 +3,20 @@ require "spec_helper"
 describe "user can submit story link" do
   let(:user) { FactoryGirl.create(:user) }
   let(:story) { Story.new(link: "example.com") }
+
+  it "can create a new story" do 
+  
+
+  end
+
+
+
+
+  def login(user)
+    visit '/login'
+    fill :username, with: user.username
+    fill_in :password, user.password
+    click_button "Log In"
+  end
+
 end
