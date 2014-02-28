509cea89da726f3064b97bbbddd69c255b4df7c4
Sahil Trikha <trikha93@gmail.com>
2014-02-28 11:22:19 -0500

    set up schema

--- db/migrate/20140228161400_schema_setup.rb
+++ db/migrate/20140228161400_schema_setup.rb
@@ -1,4 +1,26 @@
 class SchemaSetup < ActiveRecord::Migration
   def change
+      create_table :users do |t|
+        t.string :username
+        t.string :email
+        t.string :password
+      end
+      create_table :stories do |t|
+        t.text :link
+        t.string :title
+        t.timestamps
+      end
+      create_table :votes do |t|
+        t.references :user
+        t.references :story
+        t.integer :vote
+        t.timestamps
+      end
+      create_table :comments do |t|
+        t.references :user
+        t.references :story
+        t.text :comment
+        t.timestamps
+      end
   end
 end
