2b6deba2b12be75a8d89ded2b88f132665b28c49
Sahil Trikha <trikha93@gmail.com>
2014-02-28 10:53:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redit::Application.config.secret_key_base = '320a1b5df854dc18ac9cbd5e1cfed6ab16dee9061053fbcd84920324780c76c51de35ecaed11ffc87649d51d68cf0a797e36308801a5bb1b147f6ed74f356fab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Redit::Application.config.session_store :cookie_store, key: '_redit_session'
